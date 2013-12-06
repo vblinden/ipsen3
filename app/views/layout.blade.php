bcaaffb9c2265c0b871fca9a62a0c50701499bcf
Vincent van der Linden <vblinden@outlook.com>
2013-12-06 20:25:00 +0100

    Fixes #5

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -15,136 +15,139 @@
 </head>
 
 <body>
-	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="/">LeenMeij</a>
-			</div>
-			<div class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="#">Startpagina</a></li>
-					<li><a href="#">Reserveren</a></li>
-					<li><a href="#">Voertuigen</a></li>
-					<li><a href="#">Prijzen</a></li>
-					<li><a href="#">FAQ</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					@if(Auth::check())
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						@if(date("H") < "12") Goedemorgen,
-						@elseif(date("H") < "18") Goedemiddag,
-						@else Goedenavond,
-						@endif
-						{{ Auth::user()->firstname; }}<b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="/user/account">Mijn account</a></li>
-							@if(Role::find(Auth::user()->role['role_id'])['name'] == 'admin') <li><a href='/admin/'>Administrator</a></li>@endif
-							<li class="divider"></li>
-							<li><a href="/user/logout">Uitloggen</a></li>
+	<div class="wrapper">
+		<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/">LeenMeij</a>
+				</div>
+				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Startpagina</a></li>
+						<li><a href="#">Reserveren</a></li>
+						<li><a href="#">Voertuigen</a></li>
+						<li><a href="#">Prijzen</a></li>
+						<li><a href="#">FAQ</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						@if(Auth::check())
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								@if(date("H") < "12") Goedemorgen,
+								@elseif(date("H") < "18") Goedemiddag,
+								@else Goedenavond,
+								@endif
+								{{ Auth::user()->firstname; }}<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="/user/account">Mijn account</a></li>
+									@if(Role::find(Auth::user()->role['role_id'])['name'] == 'admin') <li><a href='/admin/'>Administrator</a></li>@endif
+									<li class="divider"></li>
+									<li><a href="/user/logout">Uitloggen</a></li>
+								</ul>
+							</li>
+							@else
+							<li><a href="/user/login">Inloggen</a></li>
+							<li><a href="/user/register">Registreren</a></li>
+							@endif
 						</ul>
-					</li>
-					@else
-					<li><a href="/user/login">Inloggen</a></li>
-					<li><a href="/user/register">Registreren</a></li>
-					@endif
-				</ul>
+					</div>
+				</div>
 			</div>
+
+			<div class="container">
+				@yield('content')
+			</div>
+			<div class="push"></div>
 		</div>
-	</div>
 
-	<div class="container">
-		@yield('content')
-	</div>
-
-	<div id="footer" style="padding-top: 20px; margin-top: 20px;">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-4">
-					<div class="panel panel-default">
-						<div class="panel-heading">LeenMeij</div>
-						<div class="panel-body">
-							<ul class="bulletless">
-								<li><a href="#">Reseveer nu</a></li>
-								<li><a href="#">Langer huren</a></li>
-								<li><a href="#">Extra services</a></li>
-								<li><a href="#">Vestigingen</a></li>
-								<li><a href="#">Wagenpark</a></li>
-								<li><a href="#">Over LeenMeij</a></li>
-							</ul>
+		<div id="footer" style="padding-top: 20px; margin-top: 20px;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4">
+						<div class="panel panel-default">
+							<div class="panel-heading">LeenMeij</div>
+							<div class="panel-body">
+								<ul class="bulletless">
+									<li><a href="#">Reseveer nu</a></li>
+									<li><a href="#">Langer huren</a></li>
+									<li><a href="#">Extra services</a></li>
+									<li><a href="#">Vestigingen</a></li>
+									<li><a href="#">Wagenpark</a></li>
+									<li><a href="#">Over LeenMeij</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="panel panel-default">
+							<div class="panel-heading">Diensten</div>
+							<div class="panel-body">
+								<ul class="bulletless">
+									<li><a href="#">Personenauto</a></li>
+									<li><a href="#">Bedrijfswagens</a></li>
+									<li><a href="#">Motoren</a></li>
+									<li><a href="#">Scooters</a></li>
+									<li><a href="#">Accessoires</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="panel panel-default">
+							<div class="panel-heading">Contact</div>
+							<div class="panel-body" style="padding: 0px;">
+								<div id="footer-maps" style="height: 200px;"></div>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-4">
-					<div class="panel panel-default">
-						<div class="panel-heading">Diensten</div>
-						<div class="panel-body">
-							<ul class="bulletless">
-								<li><a href="#">Personenauto</a></li>
-								<li><a href="#">Bedrijfswagens</a></li>
-								<li><a href="#">Motoren</a></li>
-								<li><a href="#">Scooters</a></li>
-								<li><a href="#">Accessoires</a></li>
-							</ul>
-						</div>
+				<div class="row">
+					<div class="col-lg-12">
+						<p>Copyright &copy; <?php echo date("Y"); ?> LeenMeij. Alle rechten voorbehouden. Gemaakt door: Koning Keizer Admiraal Hofnar</p>
 					</div>
-				</div>
-				<div class="col-lg-4">
-					<div class="panel panel-default">
-						<div class="panel-heading">Contact</div>
-						<div class="panel-body" style="padding: 0px;">
-							<div id="footer-maps" style="height: 200px;"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-12">
-					<p>Copyright &copy; <?php echo date("Y"); ?> LeenMeij. Alle rechten voorbehouden. Gemaakt door: Koning Keizer Admiraal Hofnar</p>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<script src="/js/jquery.js"></script>
-	<script src="/js/bootstrap.js"></script>
-	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-	<script src="/js/gmaps.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function () {
-			var map = new GMaps({
-				div: '#footer-maps',
-				lat: 52.16662,
-				lng: 4.47184
+		<script src="/js/jquery.js"></script>
+		<script src="/js/bootstrap.js"></script>
+		<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+		<script src="/js/gmaps.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function () {
+				var map = new GMaps({
+					div: '#footer-maps',
+					lat: 52.16662,
+					lng: 4.47184
+				});
+
+				var mapOptions = {
+					draggable: false,
+					scrollwheel: false,
+					panControl: false,
+					zoomControl: false,
+					scaleControl: false,
+					mapTypeControl: false,
+					streetViewControl: false,
+					overviewMapControl: false
+				};
+
+				map.setOptions(mapOptions);
+
+				map.addMarker({
+					lat: 52.16662,
+					lng: 4.47184,
+					infoWindow: {
+						content: '<p>Nepstraat 123<br/>2233AB Leiden<br/>010-1923994<br/>info@leenmeij.tk</br></p>'
+					}
+				});
 			});
-
-			var mapOptions = {
-				draggable: false,
-				scrollwheel: false,
-				panControl: false,
-				zoomControl: false,
-				scaleControl: false,
-				mapTypeControl: false,
-				streetViewControl: false,
-				overviewMapControl: false
-			};
-
-			map.setOptions(mapOptions);
-
-			map.addMarker({
-				lat: 52.16662,
-				lng: 4.47184,
-				infoWindow: {
-					content: '<p>Nepstraat 123<br/>2233AB Leiden<br/>010-1923994<br/>info@leenmeij.tk</br></p>'
-				}
-			});
-		});
-	</script>
-</body>
-</html>
+		</script>
+	</body>
+	</html>
