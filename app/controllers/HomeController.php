43f6c900f02059e32bb3754294b5b9eeb042e15c
Vincent van der Linden <vblinden@outlook.com>
2013-12-12 10:49:21 +0100

    Fixes #31

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,16 +2,6 @@
 
 class HomeController extends BaseController {
 
-	public function __construct() 
-	{
-		// TEST: Protect this controller from not logged in users, this needs to be placed in
-		// controllers which needs to protected from guest access. If some methods are public
-		// you can use the except function (check the docs).
-		$this->beforeFilter('auth', array(
-			'except' => 'getIndex'
-		));
-	}
-
 	public function getIndex()
 	{
 		return View::make('index');
