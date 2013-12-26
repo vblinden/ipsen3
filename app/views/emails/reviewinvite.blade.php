bcb41d6202eae4b49bc529cbc6c2147c8ada369e
vblinden <vbvanderlinden@gmail.com>
2013-12-26 20:30:43 +0100

    Added reservation review email functionality

--- app/views/emails/reviewinvite.blade.php
+++ app/views/emails/reviewinvite.blade.php
@@ -6,6 +6,19 @@
 	<body>
 		<div>
 			<p>Beste {{{ $user->firstname }}},</p>
+
+			<p>
+				Het is ondertussen alweer 5 dagen geleden dat u uw voertuig heeft terug gebracht. Wij zijn 
+				benieuwd wat deze ervaring bij u terweeg heeft gebracht, daarom willen wij u vragen om een 
+				beoordeling te geven. In deze email staan 2 links voor het maken van een bedrijfsbeoordeling 
+				en een voertuigbeoordeling. dit zal hoogstens 5 minuten van uw tijd in beslag nemen, en kan 
+				ons helpen om de klantvriendelijkheid en kwaliteit van het bedrijf te verbeteren
+			</p>
+
+			<p>
+				<a href="#" target="_blank">Klik hier</a> om een voertuigbeoordeling te maken. <br/>
+				<a href="#" target="_blank">Klik hier</a> om een bedrijfbeoordeling te maken. 
+			</p>
 	
 			<p>Met vriendelijke groet,<p>
 			<p>LeenMeij Voertuigverhuur BV.</p>
