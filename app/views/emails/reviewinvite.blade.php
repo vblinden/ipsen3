83e255f79c165263c92bc6dbd2cd0b6e8fbfecde
vblinden <vbvanderlinden@gmail.com>
2013-12-26 14:41:00 +0100

    Changed review invite mail

--- app/views/emails/reviewinvite.blade.php
+++ app/views/emails/reviewinvite.blade.php
@@ -6,30 +6,9 @@
 	<body>
 		<div>
 			<p>Beste {{{ $user->firstname }}},</p>
-			<p>
-				Hartelijk dank voor je registratie bij voertuigverhuurbedrijf LeenMeij. LeenMeij is een
-				van de grootste aanbieders voor voertuigverhuur in de regio Leiden en hopen u van dienst te
-				kunnen zijn bij het zoeken naar een voertuig. 
-			</p>
-			<p>
-				Je kunt nu inloggen op <a href="http://leenmeij.vblinden.com" target="_blank">de LeenMeij website</a> met
-				de volgende gegevens.
-			</p>
-			<p>
-				<strong>E-mail:</strong> {{ $user->email }}<br/>
-				<strong>Wachtwoord:</strong> Uw persoonlijk gekozen wachtwoord.
-			</p>
-
-			<p>
-				Veel plezier met uw toekomstige gehuurde voertuigen!
-			</p>
-
-			<p>
-				Met vriendelijke groet,
-			<p>
-			<p>
-				LeenMeij Voertuigverhuur BV.
-			</p>
+	
+			<p>Met vriendelijke groet,<p>
+			<p>LeenMeij Voertuigverhuur BV.</p>
 		</div>
 	</body>
 </html>
