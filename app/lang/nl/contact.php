438495202a319c477b5dabfe3816423fdf0d41bc
Michiel Mooring <michielmooring@gmail.com>
2014-01-17 23:05:49 +0100

    Translation dutch complete
    Nederlandse vertaling is nu volledig gebeurt

---
+++ app/lang/nl/contact.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	'contactTitle' => 'Contact',
+	'contactSubtitle' => 'opnemen met LeenMeij',
+	'contactInfo' => 'Hieronder vind u informatie om op meerdere manieren contact met ons op te nemen.',
+);
---
+++ app/lang/nl/reservation.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'rentTitle' => 'Reserveren',
+	'startDateLabel' => 'Start datum',
+	'endDateLabel' => 'Eind datum',
+	'btnShowVehicles' => 'Laat beschikbare voertuigen zien',
+	'vehicleCategory' => 'Voertuig categorie',
+	'vehicleOption1' => 'Personenauto',
+	'vehicleOption2' => 'Bedrijfswagen',
+	'vehicleOption3' => 'Motor',
+	'vehicleOption4' => 'Scooter'
+);
