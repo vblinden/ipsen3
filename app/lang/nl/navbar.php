df22fbca50b4937eb13b1842c4ffb3076616e00f
Michiel Mooring <michielmooring@gmail.com>
2014-01-17 16:43:03 +0100

    Translation
    Splitted footer and navbar in different files. Index is fully translated

---
+++ app/lang/nl/navbar.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'navHome' => 'Startpagina',
+	'navRent' => 'Reserveren',
+	'navVehicles' => 'Voertuigen',
+	'navPassengerCar' => 'Personenauto',
+	'navCommercialVehicle' => 'Bedrijfswagen',
+	'navMotorcycle' => 'Motor',
+	'navScooter' => 'Scooter',
+	'navAccessories' => 'Accessoires',
+	'navRatings' => 'Beoordelingen',
+	'navFAQ' => 'FAQ',
+	'navLogin' => 'Inloggen',
+	'navRegister' => 'Registreren',
+	'navPrivate' => 'Particulier',
+	'navCommercial' => 'Zakelijk'
+);
